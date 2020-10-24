ebe7312bde5742749bd79a01fe4ed08774be8623
David <david.windler@windowslive.com>
2020-10-25 00:01:20 +0200

    List of stocks on stock list page

--- src/app/features/stock-list/components/stock-list/stock-list.component.ts
+++ src/app/features/stock-list/components/stock-list/stock-list.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StoreService } from 'src/app/core/services/store.service';
+import { SavedSelectedStock } from 'src/app/features/stock-search/models/saved-selected-stock.model';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
+  styleUrls: ['./stock-list.component.sass'],
 })
-export class StockListComponent {}
+export class StockListComponent implements OnInit {
+  public selectedStocks$: Observable<SavedSelectedStock[]>;
+
+  constructor(private store: StoreService) {}
+
+  ngOnInit(): void {
+    this.selectedStocks$ = this.store.getSelectedStocks();
+  }
+}
--- src/app/features/stock-list/stock-list.module.ts
+++ src/app/features/stock-list/stock-list.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { StockListComponent } from './components/stock-list/stock-list.component';
 import { StockListPageComponent } from './pages/stock-list-page/stock-list-page.component';
 import { StockListRoutingModule } from './stock-list-routing.module';
 
 @NgModule({
   declarations: [StockListPageComponent, StockListComponent],
-  imports: [StockListRoutingModule],
+  imports: [SharedModule, StockListRoutingModule],
   providers: [],
 })
 export class StockListModule {}
