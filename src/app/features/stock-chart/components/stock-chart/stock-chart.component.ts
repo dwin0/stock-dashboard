603b64c71e7437e92d5a5ae3c9ff0d3cfc01a77c
David <david.windler@windowslive.com>
2020-10-25 13:10:52 +0100

    Show stock prices in chart

--- src/app/features/stock-chart/components/stock-chart/stock-chart.component.ts
+++ src/app/features/stock-chart/components/stock-chart/stock-chart.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { Subscription } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { StoreService } from 'src/app/core/services/store.service';
 import { StockTimeSeriesService } from '../../services/stock-time-series.service';
@@ -7,31 +10,52 @@ import { StockTimeSeriesService } from '../../services/stock-time-series.service
   selector: 'app-stock-chart',
   templateUrl: './stock-chart.component.html',
 })
-export class StockChartComponent implements OnInit {
+export class StockChartComponent implements OnInit, OnDestroy {
+  private lineChartDataSubscription: Subscription;
+
+  public lineChartData: ChartDataSets[];
+  public lineChartLabels: Label[];
+
+  public readonly lineChartOptions = {
+    responsive: true,
+  };
+
   constructor(private stockTimeSeries: StockTimeSeriesService, private store: StoreService) {}
 
   public ngOnInit(): void {
-    this.store
+    this.lineChartDataSubscription = this.store
       .getSelectedStocks$()
       .pipe(
         switchMap((stocks) => this.stockTimeSeries.getTimeSeries(stocks)),
         map((timeSeries) =>
-          timeSeries.reduce((acc, ts) => {
+          timeSeries.map((ts) => {
             const symbol = ts['Meta Data']['2. Symbol'];
 
-            const closingPrices = Object.entries(ts['Time Series (Daily)'])
-              .map(([date, values]) => ({
-                [date]: values['4. close'],
-              }))
+            const dates = Object.keys(ts['Time Series (Daily)']).reverse();
+
+            const closingPrices = Object.values(ts['Time Series (Daily)'])
+              .map((dayValue) => Number(dayValue['4. close']))
               .reverse();
 
             return {
-              ...acc,
-              [symbol]: closingPrices,
+              symbol,
+              dates,
+              closingPrices,
             };
-          }, {})
+          })
         )
       )
-      .subscribe(console.log);
+      .subscribe((timeSeriesData) => {
+        this.lineChartData = timeSeriesData.map((tsData) => ({
+          data: tsData.closingPrices,
+          label: tsData.symbol,
+        }));
+
+        this.lineChartLabels = timeSeriesData[0].dates;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.lineChartDataSubscription.unsubscribe();
   }
 }
