305fa1fcff5c3ea0fdbb2f116eb805f4922bea04
David <david.windler@windowslive.com>
2020-10-25 12:00:15 +0100

    Read time series from API

--- src/app/features/stock-chart/components/stock-chart/stock-chart.component.ts
+++ src/app/features/stock-chart/components/stock-chart/stock-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { StoreService } from 'src/app/core/services/store.service';
 import { StockTimeSeriesService } from '../../services/stock-time-series.service';
 
@@ -11,9 +11,27 @@ export class StockChartComponent implements OnInit {
   constructor(private stockTimeSeries: StockTimeSeriesService, private store: StoreService) {}
 
   public ngOnInit(): void {
-    const timeSeries$ = this.store
+    this.store
       .getSelectedStocks$()
-      .pipe(switchMap((stocks) => this.stockTimeSeries.getTimeSeries(stocks)))
+      .pipe(
+        switchMap((stocks) => this.stockTimeSeries.getTimeSeries(stocks)),
+        map((timeSeries) =>
+          timeSeries.reduce((acc, ts) => {
+            const symbol = ts['Meta Data']['2. Symbol'];
+
+            const closingPrices = Object.entries(ts['Time Series (Daily)'])
+              .map(([date, values]) => ({
+                [date]: values['4. close'],
+              }))
+              .reverse();
+
+            return {
+              ...acc,
+              [symbol]: closingPrices,
+            };
+          }, {})
+        )
+      )
       .subscribe(console.log);
   }
 }
