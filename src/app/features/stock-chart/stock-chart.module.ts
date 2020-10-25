248eda371d4a113638f46e90b72ec77dcf50ea83
David <david.windler@windowslive.com>
2020-10-25 12:04:27 +0100

    install charts library

--- src/app/features/stock-chart/stock-chart.module.ts
+++ src/app/features/stock-chart/stock-chart.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { StockChartComponent } from './components/stock-chart/stock-chart.component';
 import { StockChartPageComponent } from './pages/stock-chart-page/stock-chart-page.component';
@@ -6,7 +7,7 @@ import { StockChartRoutingModule } from './stock-chart-routing.module';
 
 @NgModule({
   declarations: [StockChartPageComponent, StockChartComponent],
-  imports: [SharedModule, StockChartRoutingModule],
+  imports: [SharedModule, StockChartRoutingModule, ChartsModule],
   providers: [],
 })
 export class StockChartModule {}
